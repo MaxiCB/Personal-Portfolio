2820a1b952b129b42d03fb97399ffbac9acbd8ed
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-15 11:41:31 -0500

    ADD API && Connect Vercel
    - Added initial base API call for serverless functions
    - Connected project to vercel using CLI

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="https://avatars1.githubusercontent.com/u/50126858?s=460&u=db276bcd977af938c67e3f01ada094513f2c2ee7&v=4"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -16,3 +19,11 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+img {
+  width: 260px;
+  height: 260px;
+  border-radius: 20%;
+}
+</style>
