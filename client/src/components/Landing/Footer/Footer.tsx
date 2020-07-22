77217d8a244448c911a1482e9d6ea9bd13876cc3
Aaron Beasley <aaroncb@aaroncb.com>
2020-07-22 13:36:35 -0500

    ADD Footer Mobile First
    - Added media queries for footer
    - Moved all footer styling to separate css file

--- client/src/components/Landing/Footer/Footer.tsx
+++ client/src/components/Landing/Footer/Footer.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 // Style
-import "../index.css";
+import "../../../styles/footer.css";
 import Mouse from "./Mouse";
 import Accolade from "./Accolade";
+// Icons
+import { ChevronsDown } from "react-feather";
 
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="mobile-footer">
+        <h1>Scroll Down</h1>
+        <ChevronsDown className="chevrons" />
+      </div>
       <Mouse />
       <Accolade />
     </div>
